b945cc790d7c09585bb889a9f1b3c35a7e8ba2fd
Logof <logof2008@mail.ru>
2021-07-24 12:09:51 +0500

    Homework - Java Testing System

---
+++ src/HomeWork.java
@@ -0,0 +1,16 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.util.Base64;
+import java.util.Scanner;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter you full name");
+        String fullName = sc.nextLine();
+
+        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
+        System.out.println("Program result: " + encodedFullName);
+        System.out.println("Decode result: " + new String(Base64.getDecoder().decode(encodedFullName.getBytes())));
+    }
+}
