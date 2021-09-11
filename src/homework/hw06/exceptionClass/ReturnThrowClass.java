3cae6acac26575914f33b940aafebbd88d021122
Logof <logof2008@mail.ru>
2021-09-11 12:04:48 +0500

    реставрация проекта

--- src/homework/hw06/exceptionClass/ReturnThrowClass.java
+++
@@ -1,7 +0,0 @@
-package homework.hw06.exceptionClass;
-
-public class ReturnThrowClass {
-    public String getStringLine() throws IllegalAccessException {
-        throw new IllegalAccessException("Я упал");
-    }
-}
---
+++ src/main/java/homework/hw_money/separator/ISeparatorDescription.java
@@ -0,0 +1,5 @@
+package homework.hw_money.separator;
+
+public interface ISeparatorDescription {
+    public String getSeparatorDescription(long number, Separators separator);
+}
